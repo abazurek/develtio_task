abaf455159aa2506dc27f8967315987505f517a4
abazurek <skala2294@gmail.com>
2020-11-06 19:05:33 +0100

    created order section without good styled paraghaph

--- src/components/main/Order.js
+++ src/components/main/Order.js
@@ -2,9 +2,29 @@ import React from 'react';
 
 export default function Order() {
 
-    return(
-        <>
-            <h1>Order</h1>
-        </>
+    return (
+        <div className='order-section'>
+            <div className='container'>
+                <div className='text-box'>
+                    <h2>Ubezpieczenia majątkowe</h2>
+                    <h5>Co obejmują?</h5>
+                    <p>
+                        <span>
+                        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
+                        sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.
+                        Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+                        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor
+                        invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam
+                        et justo duo dolores et ea rebum.
+                    </span>
+                        Stet clita kasd gubergren, no sea takimata sanctus est
+                        Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet, consetetur sadipscing elitr.</p>
+                </div>
+                <div className='button-box'>
+                    <button>Zamów ubezpieczenie</button>
+                </div>
+            </div>
+
+        </div>
     )
 }
