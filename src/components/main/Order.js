e73d29a7cf98ab35e36bf5e49b351c8372af1912
abazurek <skala2294@gmail.com>
2020-11-06 19:52:16 +0100

    fixed paraghraph color

--- src/components/main/Order.js
+++ src/components/main/Order.js
@@ -16,7 +16,7 @@ export default function Order() {
                         Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor
                         invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam
                         et justo duo dolores et ea rebum.
-                    </span>
+                    </span> <br/>
                         Stet clita kasd gubergren, no sea takimata sanctus est
                         Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet, consetetur sadipscing elitr.</p>
                 </div>
