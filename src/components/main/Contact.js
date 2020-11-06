fb6558fb8d50bce5c75d03845ab5cbfd382add84
abazurek <skala2294@gmail.com>
2020-11-06 16:29:48 +0100

    started contact section changes

--- src/components/main/Contact.js
+++ src/components/main/Contact.js
@@ -3,8 +3,39 @@ import React from 'react';
 export default function Contanct() {
 
     return(
-        <>
-            <h1>Contact</h1>
-        </>
+        <div className='contact-section'>
+            <div className='blue-box'>
+                <div className='container text-box'>
+                    <h3>Ubezpieczenie majątkowe</h3>
+                    <p>Skontaktuj się z nami i skorzystaj ze specjalnej oferty!</p>
+                </div>
+            </div>
+            <div className='container'>
+                <div className='form-box'>
+                    <p>Podaj dane kontaktowe, a my przygotujemy najlepszą ofertę</p>
+                    <form>
+                        <label>
+                            <input type='text' placeholder='Imię i nazwisko'/>
+                        </label>
+                        <label>
+                            <input type='email' placeholder='Adres e-mail'/>
+                        </label>
+                        <label>
+                            <input type='phone' placeholder='Nr telefonu'/>
+                        </label>
+
+                            <div>
+                                <label>
+                                    <input type='text' placeholder='Miejscowość'/>
+                                </label>
+                                <label>
+                                    <input type='text' placeholder='Miejscowość'/>
+                                </label>
+                            </div>
+
+                    </form>
+                </div>
+            </div>
+        </div>
     )
 }
