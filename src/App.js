c1f932b99f43cb70a61023272ccacf9e3bc22bd1
abazurek <skala2294@gmail.com>
2020-11-06 13:40:20 +0100

    project configuration

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React from 'react';
 
+import Header from "./components/Header";
+import Main from "./components/main/Main";
+import Footer from "./components/Footer";
+
 function App() {
   return (
     <div className="App">
+      <Header/>
+      <Main/>
+      <Footer/>
     </div>
   );
 }
