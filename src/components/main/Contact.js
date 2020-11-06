e77543411bf75960ad1c83e0bb3a5d5f036fc870
abazurek <skala2294@gmail.com>
2020-11-06 18:20:28 +0100

    finished contact section

--- src/components/main/Contact.js
+++ src/components/main/Contact.js
@@ -1,41 +1,26 @@
 import React from 'react';
+import Form from "./Form";
 
 export default function Contanct() {
 
-    return(
+    return (
         <div className='contact-section'>
-            <div className='blue-box'>
-                <div className='container text-box'>
-                    <h3>Ubezpieczenie majątkowe</h3>
-                    <p>Skontaktuj się z nami i skorzystaj ze specjalnej oferty!</p>
-                </div>
-            </div>
             <div className='container'>
                 <div className='form-box'>
                     <p>Podaj dane kontaktowe, a my przygotujemy najlepszą ofertę</p>
-                    <form>
-                        <label>
-                            <input type='text' placeholder='Imię i nazwisko'/>
-                        </label>
-                        <label>
-                            <input type='email' placeholder='Adres e-mail'/>
-                        </label>
-                        <label>
-                            <input type='phone' placeholder='Nr telefonu'/>
-                        </label>
-
-                            <div>
-                                <label>
-                                    <input type='text' placeholder='Miejscowość'/>
-                                </label>
-                                <label>
-                                    <input type='text' placeholder='Miejscowość'/>
-                                </label>
-                            </div>
-
-                    </form>
+                    <Form/>
                 </div>
             </div>
+            <div className='blue-box'>
+                <div className='container '>
+                    <div className='text-box'>
+                        <h3>Ubezpieczenie majątkowe</h3>
+                        <p>Skontaktuj się z nami i skorzystaj ze specjalnej oferty!</p>
+                    </div>
+
+                </div>
+            </div>
+
         </div>
     )
 }
