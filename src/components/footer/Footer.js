a605251a93dd608b93d576f725275009f44fb0f3
abazurek <skala2294@gmail.com>
2020-11-07 18:10:13 +0100

    finished footer section for media queries rules

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,20 +1,24 @@
 import React from 'react';
 import List from "./List";
 
-export default function Footer() {
+export default function Footer({isExtraSmall, isPhone}) {
 
     return (
         <footer className='footer-section'>
             <div className='container'>
                 <List title='O nas' li1='Strona główna' li2="O nas" li3='Dołącz do nas' li4='Zostań Agentem'
-                      li5='Kariera'/>
+                      li5='Kariera' isExtraSmall={isExtraSmall} isPhone={isPhone}/>
                 <List title='Ubezpieczenia' li1='Ubezpieczenia mienia, nieruchomości'
                       li2="Ubezpiecznie osobowe" li3='Ubezpiecznie rolne' li4='Ubezpiecznie OC'
-                      li5='Zobacz pełną ofertę'/>
+                      li5='Zobacz pełną ofertę' isExtraSmall={isExtraSmall} isPhone={isPhone}/>
                 <List title='Pomoc' li1='Oddziały/Kontakt'
                       li2="Towarzystwa ubezpieczeniowe" li3='Przydatne materiały' li4='Informacje Administracyjne'
-                      li5='Polityka cookies'/>
-                      <List title='infolinia' li1='+48 22 311 1234'/>
+                      li5='Polityka cookies' isExtraSmall={isExtraSmall} isPhone={isPhone}/>
+                <ul>
+                    <p>infolinia</p>
+                    <li>+48 22 311 1234</li>
+                </ul>
+
 
             </div>
         </footer>
--- src/components/footer/List.js
+++ src/components/footer/List.js
@@ -1,15 +1,34 @@
-import React from 'react';
+import React,{useState} from 'react';
+import {faAngleDoubleDown} from "@fortawesome/free-solid-svg-icons";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 
-export default function List({title,li1, li2, li3, li4, li5}) {
+export default function List({title,li1, li2, li3, li4, li5, isExtraSmall, isPhone}) {
+
+    const[active,setActive]=useState(false);
+
+    const list=[li1, li2, li3, li4, li5];
+
+        function showList() {
+            setActive(!active)
+        }
+
+        function looseActive() {
+            setActive(false);
+        }
+
 
     return(
         <ul>
             <p>{title}</p>
-            <li>{li1}</li>
-            <li>{li2}</li>
-            <li>{li3}</li>
-            <li>{li4}</li>
-            <li>{li5}</li>
+            {(isExtraSmall || isPhone)?
+                <div >
+                    <FontAwesomeIcon icon={faAngleDoubleDown} onClick={showList}/>
+                    {active? list.map(item=><li key={list.indexOf(item)} onClick={looseActive}>{item}</li>):''}
+                </div>
+            :
+               list.map(item => <li key={list.indexOf(item)}>{item}</li>)
+            }
+
         </ul>
     )
 }
